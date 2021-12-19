137878572c5046e85eeac4f80d72e1048548b337
nadyapermaata <79756684+nadyapermaata@users.noreply.github.com>
2021-12-19 23:25:28 +0700

    Add files via upload

---
+++ src/Main.java
@@ -0,0 +1,14 @@
+
+public class Main {
+	public static void main(String[] args) {
+		javax.swing.SwingUtilities.invokeLater(new Runnable() {
+			@Override
+			public void run() {
+				// TODO Auto-generated method stub
+				MainFrame Chess = new MainFrame();
+				Chess.setVisible(true);
+			}
+		});
+	}
+}
+	
---
+++ src/MainFrame.java
@@ -0,0 +1,97 @@
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.*;
+
+public class MainFrame extends JFrame implements ActionListener{
+	private static final long serialVersionUID = 1L;
+	static int humanAsWhite = -1;
+	static int turn = 0;
+	static int mode;
+	UserInterface ui = new UserInterface();
+	String[] gameMode = {"Normal Chess", "Deschess"};
+	Object[] option={"Computer","Me"};
+	private JButton reset;
+	private JPanel toolboxPanel, toolboxPadding;
+	
+	public MainFrame() {
+		super("DesCess");
+		
+		Image image = new ImageIcon("Logo.png").getImage();
+		Icon icon = new ImageIcon(image);
+		
+		JOptionPane.showOptionDialog(null, 
+				null,
+		        "DesCess", 
+		        JOptionPane.OK_CANCEL_OPTION, 
+		        JOptionPane.INFORMATION_MESSAGE, 
+		        icon, 
+		        new String[]{"Start"},
+		        "Start");
+		
+		
+		
+		mode = JOptionPane.showOptionDialog(null, 
+				"Choose Game Mode", 
+				"Game Mode", 
+				JOptionPane.YES_NO_OPTION, 
+				JOptionPane.QUESTION_MESSAGE,
+				null,
+				gameMode, 
+				gameMode[0]);
+		
+		
+		AlphaBetaChess.init(mode);
+	
+		reset = new JButton("Reset");
+		toolboxPanel = new JPanel();
+		toolboxPanel.setLayout(new GridLayout(1, 1, 1, 1));
+		toolboxPadding = new JPanel();
+		toolboxPadding.setLayout(new FlowLayout(FlowLayout.LEADING, 1, 1));
+
+		toolboxPanel.add(reset);
+		toolboxPadding.add(toolboxPanel);
+		add(toolboxPadding, BorderLayout.NORTH);
+		add(ui, BorderLayout.CENTER);
+
+		reset.addActionListener(this);
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		humanAsWhite=JOptionPane.showOptionDialog(null, "Who plays first?", "Options", JOptionPane.YES_NO_OPTION,
+                JOptionPane.QUESTION_MESSAGE, null, option, option[1]);
+		if (humanAsWhite==0) {
+            AlphaBetaChess.makeMove(AlphaBetaChess.alphaBeta(AlphaBetaChess.globalDepth, 1000000, -1000000, "", 0));
+            AlphaBetaChess.flipBoard();
+            turn++;
+            repaint();
+        }
+		
+		setSize(526, 576);
+		setVisible(true);		
+	}
+
+		@Override
+		public void actionPerformed(ActionEvent event) {
+			if (event.getSource() == reset) {
+				if(mode == 0) {
+					for(int i = 0; i < AlphaBetaChess.normalChessBoard.length ; i++) {
+						AlphaBetaChess.chessBoard[i] = AlphaBetaChess.normalChessBoard[i].clone();
+					}
+				}
+				else if (mode == 1) {
+					for(int i = 0; i < AlphaBetaChess.desChessBoard.length ; i++) {
+						AlphaBetaChess.chessBoard[i] = AlphaBetaChess.desChessBoard[i].clone();
+					}
+				}
+				repaint();
+				
+				if (humanAsWhite == 0) {
+					AlphaBetaChess.makeMove(AlphaBetaChess.alphaBeta(AlphaBetaChess.globalDepth, 1000000, -1000000, "", 0));
+		            AlphaBetaChess.flipBoard();
+		            turn++;
+				}
+				repaint();
+			}
+		}
+	}
+
